7f8f670811bcf959f82438034180827438bb6fef
moeyua <moeyua@foxmail.com>
2021-09-27 17:49:38 +0800

    first commit

---
+++ preload.js
@@ -0,0 +1,42 @@
+var key = 'https://api.day.app/f8VKmJyepTSSq2dFUiYsDh/'
+var content = ''
+var url = key + content;
+
+function bark() {
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function(){
+    // 通信成功时，状态值为4
+    if (xhr.readyState === 4){
+        if (xhr.status === 200){
+        console.log(xhr.responseText);
+        } else {
+        console.error(xhr.statusText);
+        }
+    }
+    };
+
+    xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+    };
+
+    xhr.open('GET', url, true);
+    xhr.send(null);
+}
+
+
+window.exports = {
+    "bark": { // 注意：键对应的是 plugin.json 中的 features.code
+       mode: "none",  // 用于无需 UI 显示，执行一些简单的代码
+       args: {
+          // 进入插件时调用
+          enter: (action) => {
+             // action = { code, type, payload }
+             window.utools.hideMainWindow()
+             // do some thing
+             bark();
+            //  window.utools.outPlugin()
+          }  
+       } 
+    }
+ }
