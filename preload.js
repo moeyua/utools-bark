04833aa4bed5e37def09c3ca238c57f31eba861e
moeyua <moeyua@foxmail.com>
2021-09-28 13:34:38 +0800

    可以自行设置 key

--- preload.js
+++ preload.js
@@ -1,47 +1,76 @@
 
 function send(url) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     
     xhr.onreadystatechange = function(){
     // 通信成功时，状态值为4
     if (xhr.readyState === 4){
         if (xhr.status === 200){
-        console.log(xhr.responseText);
+            console.log(xhr.responseText)
+            window.utools.showNotification("发送成功")
         } else {
-        console.error(xhr.statusText);
+            console.log(xhr.statusText);
         }
     }
     };
 
     xhr.onerror = function (e) {
-    console.error(xhr.statusText);
+        console.log(xhr.statusText);
     };
 
     xhr.open('GET', url, true);
     xhr.send(null);
 }
 
-// function setKey(id, key) {
-//     utools.dbStorage.setItem(id, key)
-// }
 
-// function getKey(id) {
-//     return utools.dbStorage.getItem(id)
-// }
+function getKey() {
+    let keys = window.utools.db.allDocs();
+    if (keys.length === 0) {
+        window.utools.showMessageBox({
+            type: 'warning',
+            buttons: ['我知道了'],
+            title: '警告',
+            message: '还没有设置 key',
+            defaultId: 0
+          })
+    } else{
+        return keys[0].value;
+    }
+}
 
 window.exports = {
-    "bark": { // 注意：键对应的是 plugin.json 中的 features.code
+    "send": { // 注意：键对应的是 plugin.json 中的 features.code
        mode: "none",  // 用于无需 UI 显示，执行一些简单的代码
        args: {
           // 进入插件时调用
           enter: (action) => {
-             // action = { code, type, payload }
+            // action = { code, type, payload }
              window.utools.hideMainWindow()
-             var key = 'https://api.day.app/f8VKmJyepTSSq2dFUiYsDh/';
-             var content = action.payload;
-             send(key + content);
-             window.utools.outPlugin();
-          }  
+             let key = getKey();
+             let content = action.payload;
+             send(key + content)
+            window.utools.outPlugin();
+          }
        } 
-    }
+    },
+    "setKey": { // 注意：键对应的是 plugin.json 中的 features.code
+        mode: "none",  // 用于无需 UI 显示，执行一些简单的代码
+        args: {
+            // 进入插件时调用
+            enter: (action) => {
+                // action = { code, type, payload }
+                window.utools.hideMainWindow()
+                window.utools.dbStorage.setItem("key", action.payload)
+                window.utools.outPlugin();
+            }
+        } 
+     },
+     "delKey": {
+         mode: "none",
+         args: {
+             enter: (action) => {
+                window.utools.dbStorage.removeItem("key")
+             }
+         }
+     }
  }
